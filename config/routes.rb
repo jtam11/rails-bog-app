fa58957190249670298550b7bbf31572c033523b
jtam11 <justinktam@gmail.com>
2016-05-01 16:54:19 -0700

    Created new/create route/action/view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
  root "creatures#index"
 
  get "/creatures" => "creatures#index", as: "creatures"
+ get "/creatures/new" => "creatures#new", as: "new_creature"
+ post "/creatures" => "creatures#create"
 end
