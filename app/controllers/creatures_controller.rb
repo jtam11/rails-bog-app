bb7fd229c94fddad8565c684fdd1d80ec5978d9a
jtam11 <justinktam@gmail.com>
2016-04-30 19:15:33 -0700

    Created new/create routes/action/view

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,4 +4,19 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
 
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
+private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
