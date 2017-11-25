57b43a54de76dd9a1495c2b9f8e62f46f3f3c572
jtam11 <justinktam@gmail.com>
2017-11-24 17:32:04 -0800

    Added show feature

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -15,9 +15,15 @@ class CreaturesController < ApplicationController
     creature = Creature.new(creature_params)
 
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
+  end
 
+  def show
+    creature_id = params[:id]
 
+    @creature = Creature.find_by_id(creature_id)
+
+    render :show
   end
 end
