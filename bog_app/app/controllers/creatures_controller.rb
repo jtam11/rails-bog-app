a4c30d75024123abe6ade9deef579c97823932ce
jtam11 <justinktam@gmail.com>
2017-11-24 17:21:52 -0800

    Added create feature

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -4,4 +4,19 @@ class CreaturesController < ApplicationController
 
     render :index
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
+
+
+  end
 end
